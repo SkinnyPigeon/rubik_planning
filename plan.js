39a35bcb23c8073fbd7a8a1fa5a556acf9e5b446
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-07-25 20:57:27 +0100

    possibly made working with the sides simpler

--- plan.js
+++ plan.js
@@ -1,5 +1,6 @@
 var _ = require( 'lodash' )
 
+// red
 var R1 = [ 'Y7', 'B3', 'R1' ];
 var R2 = [ 'Y8', 'R2' ];
 var R3 = [ 'Y9', 'R3', 'G1' ];
@@ -10,6 +11,7 @@ var R7 = [ 'B9', 'R7', 'W1' ];
 var R8 = [ 'R8', 'Y2' ];
 var R9 = [ 'R9', 'G7', 'W3' ];
 
+// orange
 var O1 = [ 'W7', 'B7', 'O1' ];
 var O2 = [ 'W8', 'O2' ];
 var O3 = [ 'W9', 'O3', 'G9' ];
@@ -20,45 +22,79 @@ var O7 = [ 'B1', 'O7', 'Y1' ];
 var O8 = [ 'O8', 'Y2' ];
 var O9 = [ 'O9', 'G3', 'Y3' ];
 
+// blue
+var B1 = O1
+var B2 = O4
+var B3 = O7
 var B4 = [ 'W6', 'B4' ];
 var B5 = [ 'B5' ];
 var B6 = [ 'B6', 'Y4' ];
+var B7 = R7
+var B8 = R4
+var B9 = R1
 
+// green
+var G1 = O3
+var G2 = O6
+var G3 = O9
 var G4 = [ 'Y6', 'G4' ];
 var G5 = [ 'G5' ];
 var G6 = [ 'G6', 'W4' ];
+var G7 = R3
+var G8 = R6
+var G9 = R9
 
+// yellow
+var Y1 = O7
+var Y2 = O8
+var Y3 = O9
+var Y4 = B6
 var Y5 = [ 'Y5' ];
+var Y6 = G4
+var Y7 = R1
+var Y8 = R2
+var Y9 = R3
 
+// white
+var W1 = R7
+var W2 = R7
+var W3 = R9
+var W4 = G6
 var W5 = [ 'W5' ];
+var W6 = B4
+var W7 = O1
+var W8 = O2
+var W9 = O3
 
-var front = [ R1, R2, R3, 
+var forward = [ R1, R2, R3, 
               R4, R5, R6,
               R7, R8, R9 ]
 
-var back = [ O1, O2, O3, 
+var backward = [ O1, O2, O3, 
              O4, O5, O6,
              O7, O8, O9 ]
 
-var top = [ R1, R2, R3,
+var topward = [ O7, O8, O9,
             B6, Y5, G4,
-            O7, O8, O9 ]
+            R1, R2, R3 ]
 
-var bottom = [ R7, R8, R9,
+var bottomward = [ R7, R8, R9,
                G6, W5, B4,
                O1, O2, O3 ]
 
-var left = [ O1, O4, O7,
+var leftward = [ O1, O4, O7,
              B4, B5, B6,
              R7, R4, R1 ]
 
-var right = [ O1, O4, O7,
+var rightward = [ O3, O6, O9,
               G4, G5, G6,
               R3, R6, R9 ]
 
+
+
 var test = [ 1, 2, 3, 
-4, 5, 6, 
-7, 8, 9 ]
+             4, 5, 6, 
+             7, 8, 9 ]
 
 function move(arr, fromIndex, toIndex) {
   var element = arr[fromIndex];
